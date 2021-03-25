58047cd011241c18caa79300e13d5282c391f095
barzin <barzin144@yahoo.com>
2021-03-25 14:51:41 +0430

    add hero

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header";
+import Hero from "./hero";
 
 const App = () => {
     return (
         <>
         <Header/>
+        <Hero/>
         </>
     );    
 };
---
+++ src/components/hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="container container--px flex flex-jc-sb">
+                <div className="hero__text">
+                    <h1>
+                        A Simple Bookmark Manager
+                    </h1>
+                    <p>
+                        A clean and simple interface to organize your favourite websites. Open a new
+                        browser tab and see your sites load instantly. Try it for free.
+                    </p>
+                    <div className="hero__Button">
+                        <a href="#" className="button blue-btn">Get it on Chrome</a>
+                        <a href="#" className="button gray-btn">Get it on Firefox</a>
+                    </div>
+                </div>
+                <div className="hero__image"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
