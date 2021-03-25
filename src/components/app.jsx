2be9349aea7d7258670fdb230b7b0c336a0a05cb
barzin <barzin144@yahoo.com>
2021-03-25 20:45:02 +0430

    add feature section

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Feature from "./feature";
 import Header from "./header";
 import Hero from "./hero";
 
 const App = () => {
     return (
         <>
-        <Header/>
-        <Hero/>
+            <Header />
+            <Hero />
+            <Feature />
         </>
-    );    
+    );
 };
 
 export default App;
---
+++ src/components/feature.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Feature = () => {
+    return (
+        <section className="feature">
+            <div className="container container--pall">
+                <div className="feature__text">
+                    <h1>
+                        Features
+                    </h1>
+                    <p>
+                        Our aim is to make it quick and easy for you to access your favourite websites.
+                        Your bookmarks sync between your devices so you can access them on the go.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Feature;
